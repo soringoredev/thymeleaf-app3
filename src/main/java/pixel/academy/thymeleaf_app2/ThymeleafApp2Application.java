4cbd3b9c8562255a1db01e3e6f691fb7936f4e9f
Sorin Gore <soringoredev@gmail.com>
2025-08-19 18:34:20 +0300

    Initial commit

---
+++ src/main/java/pixel/academy/thymeleaf_app2/ThymeleafApp2Application.java
@@ -0,0 +1,13 @@
+package pixel.academy.thymeleaf_app2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafApp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pixel/academy/thymeleaf_app2/ThymeleafApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pixel.academy.thymeleaf_app2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafApp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
